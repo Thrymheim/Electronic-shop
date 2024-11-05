6f32682e79a614bcb22fa70a362b52abdf4df324
Maziyar <Maziyarkolagar@gmail.com>
2024-11-05 20:28:28 +0330

    Delete Eshop_project directory

--- Eshop_project/product_madule/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class product(models.Model):
-    title = models.CharField(max_length=300) #charfield is for small amount of text unlile text field
-    price = models.IntegerField()
