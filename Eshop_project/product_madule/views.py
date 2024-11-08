fe33acf7c1a15fe7829569956a4f7470cc7ff481
Maziyar <Maziyarkolagar@gmail.com>
2024-11-08 16:57:50 +0330

    Delete Eshop_project directory

--- Eshop_project/product_madule/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import product
-
-
-def product_list(request):
-    products = product.objects.all()
-    return render(request, 'product_madule/product_list.html', {
-        'products': products
-    })
