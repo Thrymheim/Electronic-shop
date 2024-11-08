5909c7732467997c8a3bc3288530e5b1d02249cd
Maziyar <Maziyarkolagar@gmail.com>
2024-11-08 16:57:12 +0330

    Update views.py

--- Eshop_project/product_madule/views.py
+++ Eshop_project/product_madule/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import product
 
-# Create your views here.
+
+def product_list(request):
+    products = product.objects.all()
+    return render(request, 'product_madule/product_list.html', {
+        'products': products
+    })
