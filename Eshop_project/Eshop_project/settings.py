2885fef1e2b37f83e4dc56951335d9134619db5a
Maziyar <Maziyarkolagar@gmail.com>
2024-11-08 16:59:00 +0330

    Update settings.py

--- Eshop_project/Eshop_project/settings.py
+++ Eshop_project/Eshop_project/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for Eshop_project project.
 
@@ -11,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from django.core.management.utils import get_random_secret_key
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--!79(@*abf7=p+@dcnz)w65m#++qu70ploh##2xsj4n-a_^+ke'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
