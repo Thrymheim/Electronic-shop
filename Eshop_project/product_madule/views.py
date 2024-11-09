fcf6f9e93fad13f6d4f86baa43422878a9dcc015
Maziyar <Maziyarkolagar@gmail.com>
2024-11-09 20:08:44 +0330

    Update views.py

--- Eshop_project/product_madule/views.py
+++ Eshop_project/product_madule/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import product
-
+from django.http import Http404
 
 def product_list(request):
     products = product.objects.all()
     return render(request, 'product_madule/product_list.html', {
         'products': products
+    })
+def product_detail(request,product_id):
+    # try:
+    #     detail_product = product.objects.get(id=product_id)
+    # except:
+    #     raise Http404("Product does not exist")
+    detail_product=get_object_or_404(product,pk=product_id) #pk = primary key = id
+    return render(request, 'product_madule/product_detail.html', {
+        'detail_product': detail_product
     })
