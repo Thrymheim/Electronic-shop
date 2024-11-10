ab1e7a7ac4e93bc859da8f78feb1e6ae2d21c514
Maziyar <Maziyarkolagar@gmail.com>
2024-11-10 18:27:46 +0330

    Update views.py

--- Eshop_project/product_madule/views.py
+++ Eshop_project/product_madule/views.py
@@ -7,12 +7,12 @@ def product_list(request):
     return render(request, 'product_madule/product_list.html', {
         'products': products
     })
-def product_detail(request,product_id):
+def product_detail(request,slug):
     # try:
     #     detail_product = product.objects.get(id=product_id)
     # except:
     #     raise Http404("Product does not exist")
-    detail_product=get_object_or_404(product,pk=product_id) #pk = primary key = id
+    detail_product=get_object_or_404(product,slug=slug) #pk = primary key = id
     return render(request, 'product_madule/product_detail.html', {
         'detail_product': detail_product
     })
