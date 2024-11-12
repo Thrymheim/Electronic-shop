b3d963f91776e15a20a8e14099c441339fd564f8
Maziyar <Maziyarkolagar@gmail.com>
2024-11-12 20:12:30 +0330

    Update admin.py

--- Eshop_project/product_madule/admin.py
+++ Eshop_project/product_madule/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class productAdmin(admin.ModelAdmin):
+    # readonly_fields = ['slug']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    list_display = ['title', 'price','rating','is_active']
+    list_filter = ['price','rating','is_active']
+    list_editable = ['price','rating','is_active']
+admin.site.register(models.product, productAdmin )
