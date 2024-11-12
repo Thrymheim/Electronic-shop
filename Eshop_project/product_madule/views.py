081c2f0ffc4a57a821352249e8c8961083eb48fe
Maziyar <Maziyarkolagar@gmail.com>
2024-11-12 20:14:01 +0330

    Update views.py

--- Eshop_project/product_madule/views.py
+++ Eshop_project/product_madule/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import product
 from django.http import Http404
-
+from django.db.models import Avg
 def product_list(request):
-    products = product.objects.all()
+    products = product.objects.all().order_by('title') #put - before it and it becomes reverse
+    totall_number_of_products = products.count()
+    average_rating = products.aggregate(Avg('rating')) #we named it rating at models
     return render(request, 'product_madule/product_list.html', {
-        'products': products
+        'products': products,
+        'totall_number_of_products': totall_number_of_products,
+        'average_rating': average_rating
+
     })
 def product_detail(request,slug):
     # try:
