4dc0232cadf4f3165e6023ece63a84133491fb6c
Maziyar <Maziyarkolagar@gmail.com>
2024-11-13 19:29:42 +0330

    Update models.py

--- Eshop_project/product_madule/models.py
+++ Eshop_project/product_madule/models.py
@@ -3,8 +3,17 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=300, verbose_name="عنوان")
+    url_title = models.CharField(max_length=300, verbose_name='عنوان در url')
+
+    def __str__(self):
+         return self.title
+
 class product(models.Model):
     title = models.CharField(max_length=300) #charfield is for small amount of text unlile text field
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, null=True) #cascade means if category got deleted also delete the products
+    #or u can say models.protect to not delete them
     price = models.IntegerField()
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=1)
     short_description = models.CharField(max_length=360, null=True)
