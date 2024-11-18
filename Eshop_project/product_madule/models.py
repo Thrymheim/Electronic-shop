87891be08ec4d8239107667b65a2699f63977dac
Maziyar <Maziyarkolagar@gmail.com>
2024-11-18 21:53:47 +0330

    Update models.py

--- Eshop_project/product_madule/models.py
+++ Eshop_project/product_madule/models.py
@@ -9,11 +9,24 @@ class ProductCategory(models.Model):
 
     def __str__(self):
          return self.title
+    class Meta:
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی ها'
 class ProductInformation(models.Model):
     color = models.CharField(max_length=300, verbose_name='رنگ')
     size = models.CharField(max_length=300, verbose_name='سایز')
     def __str__(self):
         return f'{self.color} - {self.size}'
+    class Meta:
+        verbose_name = 'اطلاعات تکمیلی'
+        verbose_name_plural = 'تمامی اطلاعات تکمیلی'
+class ProductTags(models.Model):
+    tag = models.CharField(max_length=300, verbose_name='عنوان')
+    def __str__(self):
+        return self.tag
+    class Meta:
+        verbose_name = 'تگ محصول'
+        verbose_name_plural = 'تگ محصولات'
 
 class product(models.Model):
     title = models.CharField(max_length=300) #charfield is for small amount of text unlile text field
@@ -21,6 +34,7 @@ class product(models.Model):
                                              related_name='product', verbose_name='اطلاعات' , null=True)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, null=True) #cascade means if category got deleted also delete the products
     #or u can say models.protect to not delete them
+    product_tag = models.ManyToManyField (ProductTags, verbose_name='تگ های محصول')
     price = models.IntegerField()
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=1)
     short_description = models.CharField(max_length=360, null=True)
@@ -38,3 +52,7 @@ class product(models.Model):
 
     def __str__(self):
         return f'{self.title} ({self.price})'
+    class Meta:
+        verbose_name = 'محصول'
+        verbose_name_plural = 'محصولات'
+
