c08f6c5670a2f4ff10c0159de82ff65598e2f918
Maziyar <Maziyarkolagar@gmail.com>
2024-11-19 20:31:11 +0330

    Delete Eshop_project directory

--- Eshop_project/product_madule/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProductMaduleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'product_madule'
-    verbose_name = 'ماژول محصولات'
--- Eshop_project/product_madule/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.product_list, name='product-list'),
-    path('<slug:slug>', views.product_detail, name='product-detail'),
-]
