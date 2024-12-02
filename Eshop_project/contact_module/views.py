7f52cb65aa573efd75947db5758f084346ff9acf
Maziyar <Maziyarkolagar@gmail.com>
2024-12-02 19:55:04 +0330

    Update views.py

--- Eshop_project/contact_module/views.py
+++ Eshop_project/contact_module/views.py
@@ -24,12 +24,3 @@ class ContactUsView(View):
         return render(request, 'contact_module/contact_us_page.html', {
             'contact_form': contact_form
         })
-
-
-# def contact_us_page(request):
-#     if request.method == 'POST':
-#         contact_form = ContactUsModelForm(request.POST)
-#         if contact_form.is_valid():
-#             contact_form.save()
-#             return redirect('home_page')
-
