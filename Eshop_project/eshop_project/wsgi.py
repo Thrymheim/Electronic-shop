670cbd5ede99fde901e27c39ed43e4b45bc87bd6
Maziyar <Maziyarkolagar@gmail.com>
2025-03-07 21:08:21 +0330

    Update wsgi.py

--- Eshop_project/eshop_project/wsgi.py
+++ Eshop_project/eshop_project/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for eshop_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path = '/home/yourusername/your-portfolio-repo'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'portfolio_project.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eshop_project.settings')
-
 application = get_wsgi_application()
